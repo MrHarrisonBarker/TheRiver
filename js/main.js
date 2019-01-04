db9025834146a5c389fa392532481701ec911bb0
Harrison Barker <harrison@thebarkers.me.uk>
2019-01-04 16:37:22 +0000

    theme changer added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ $(document).ready(function () {
 
     $("#date").text(getDate());
 
+
+
+    $("#ThemeChange").click(function () {
+        if ($("main").hasClass('theme-light')){
+            $("main").attr('class', 'theme-dark');
+            $("#DarkIcon").css("display","none");
+            $("#LightIcon").css("display","unset");
+        } else {
+            $("main").attr('class', 'theme-light');
+            $("#LightIcon").css("display","none");
+            $("#DarkIcon").css("display","unset");
+        }
+    });
     
 
 });
