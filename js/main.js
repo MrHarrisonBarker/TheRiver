9c7b9db0fc2dfbfed33aa42598927d7ecde42ea2
Harrison Barker <harrison@thebarkers.me.uk>
2019-01-13 13:20:04 +0000

    modal and buttons fixed to work with dark mode

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
 
     $("#date").text(getDate());
 
-
+    $("html").attr('class', 'theme-light');
 
     $("#ThemeChange").click(function () {
-        if ($("main").hasClass('theme-light')){
-            $("main").attr('class', 'theme-dark');
+        if ($("html").hasClass('theme-light')){
+            $("html").attr('class', 'theme-dark').css("background-color", "rgb(33, 33, 33)");
             $("#DarkIcon").css("display","none");
             $("#LightIcon").css("display","unset");
         } else {
-            $("main").attr('class', 'theme-light');
+            $("html").attr('class', 'theme-light').css("background-color", "rgb(255, 255, 255)");
             $("#LightIcon").css("display","none");
             $("#DarkIcon").css("display","unset");
         }
